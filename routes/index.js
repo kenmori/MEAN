11b6a45d4cab244a947405554db51be2da068b32
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-12-31 11:08:11 +0900

    create input by Ajax

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@
  */
 
 exports.index = function(req, res){
-	var id = req.params.id;
-	var name = req.params.name;
 	res.render('index', {
 		title: 'Express',
-		msg: 'こんにちは'+ id + '番の' + name + 'さん'
+		msg: '送信してください'
 	});
 };
--- routes/index_post.js
+++ routes/index_post.js
@@ -1,6 +1,12 @@
+var data  = {
+	kenji : {mail: 'kenji@fafafa', tel: '080-9999-9999'},
+	nekochan : {mail: 'nekochan@fafafa', tel: '080-8888-8888'} 
+}
 exports.index = function(req, res){
-	res.render('index', {
-		title: 'Express',
-		msg: 'こんにちは,' + req.body.text1 + 'さん'
-	});
+	var name = req.body.name;
+	var result = data[name];
+	if(result === undefined){
+		result = {mail: '見つかりませんでした', tel: '見つかりませんでした'};
+	}
+	res.send(result);
 };
