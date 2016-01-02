06fae9d80c3abeabde77559e36a34364f09b9616
Kenji Morita <kennjimoritata@yahoo.co.jp>
2016-01-02 09:05:57 +0900

    fix query text to understand

--- routes/update.js
+++ routes/update.js
@@ -7,12 +7,8 @@ exports.update = function(req, res){
 			console.log(err);
 			res.redirect('/');
 		} else {
-			var sql = "update mynavi_db set name = '"
-				+ req.body.name + "',mail = '"
-				+ req.body.mail + "', tel = '"
-				+ req.body.tel + "' where id = "
-				+ req.body.id;
-				client.query(sql, function(err, result){
+			var sql = "update mynavi_db set name = $1, mail = $2, tel = $3 where id = $4";
+				client.query(sql, [req.body.name, req.body.mail, req.body.tel, req.body.id], function(err, result){
 					console.log(err);
 					res.redirect('/');
 				});
